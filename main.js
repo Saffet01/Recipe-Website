767a792a492702d0c82869dd2b53a35d66f0aefe
Saffet01 <saffetr.kaban@gmail.com>
2023-10-07 14:30:36 +0300

    script src and Api URL update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const breakfastTab = document.getElementById('breakfastTab');
 const lunchTab = document.getElementById('lunchTab');
 const dinnerTab = document.getElementById('dinnerTab');
 
-const APIUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=576e3078&app_key=a1148c238c69595977a20f7cebe6c2d2&mealType=${mealType}&imageSize=REGULAR`;
+const APIUrl = (mealType) => `https://api.edamam.com/api/recipes/v2?type=public&app_id=576e3078&app_key=a1148c238c69595977a20f7cebe6c2d2&mealType=${mealType}&imageSize=REGULAR`;
 
 
 function setActiveTab(tab) {
@@ -12,7 +12,6 @@ function setActiveTab(tab) {
     lunchTab.classList.remove('active');
     dinnerTab.classList.remove('active');
     tab.classList.add('active');
-    tab.style.backgroundColor = "#333"
 }
 
 allTab.addEventListener('click', function() {
@@ -37,7 +36,7 @@ dinnerTab.addEventListener('click', function() {
 });
 
 
-function loadRecipesByMealType(mealType){
+function loadRecipesByMealType(mealType = ''){
 
     fetch(APIUrl)
         .then(response => response.json())
